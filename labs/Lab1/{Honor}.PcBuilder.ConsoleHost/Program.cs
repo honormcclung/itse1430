dbb6a6010aca69f72498249445cb1ed56e675333
Honor McClung <honor.mcclung@my.tccd.edu>
2022-09-19 18:45:23 -0500

    Added display menu. Completed Lab1 stories 1-3.

--- labs/Lab1/{Honor}.PcBuilder.ConsoleHost/Program.cs
+++ labs/Lab1/{Honor}.PcBuilder.ConsoleHost/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static void Main ( string[] args )
+        {
+            Console.WriteLine("Name: Honor");
+            Console.WriteLine("Class: ISTE 1430");
+            Console.WriteLine("Date: 9/19/2022");
+
+            Test();
+            DisplayMenuOptions();
+        }
+
+        static void Test()
+        {
+            Console.WriteLine("Test Complete");
+        }
+
+        static void DisplayMenuOptions()
+        {
+            int cartTotal = 0;
+            String menuOption = "";
+            Boolean isQuitting = false;
+
+            do
+            {
+                Console.WriteLine("Cart Total: $" + cartTotal);
+                Console.WriteLine();
+
+                Console.WriteLine("Enter Menu Option.");
+                Console.WriteLine("Menu Options:");
+                Console.WriteLine("1) Quit");
+
+                menuOption = Console.ReadLine();
+            } while (menuOption != "Quit" && menuOption != "quit");
+
+            if (menuOption == "Quit")
+            {
+                Console.WriteLine("Are you sure you want to quit? (Enter Yes or No)");
+                if (Console.ReadLine() == "No")
+                {
+                    isQuitting = true;
+                } else
+                {
+                    do
+                    {
+                        Console.WriteLine("Cart Total: $" + cartTotal);
+                        Console.WriteLine();
+
+                        Console.WriteLine("Enter Menu Option.");
+                        Console.WriteLine("Menu Options:");
+                        Console.WriteLine("1) Quit");
+
+                        menuOption = Console.ReadLine();
+                    } while (menuOption != "Quit" && menuOption != "quit");
+                }
+            }
+        }
+    }
+}
