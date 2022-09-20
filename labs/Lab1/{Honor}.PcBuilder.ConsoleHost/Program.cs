b6d7025f6a339c650c38cf41c4729e4f1abb0c9b
Honor McClung <honor.mcclung@my.tccd.edu>
2022-09-19 20:30:06 -0500

    Added new order method.

--- labs/Lab1/{Honor}.PcBuilder.ConsoleHost/Program.cs
+++ labs/Lab1/{Honor}.PcBuilder.ConsoleHost/Program.cs
@@ -45,6 +45,9 @@ namespace Lab1
             if (menuOption == "1")
             {
                 QuitProgram();
+            } else if (menuOption == "2")
+            {
+                newOrder();
             }
         }
 
@@ -59,6 +62,31 @@ namespace Lab1
             {
                 Console.WriteLine("Program Quit.");
             }
+        }
+
+        static void newOrder ()
+        {
+            int cartTotal = 0;
+            String chosenProcessor = "";
+            String chosenMemory = "";
+
+            String[] processors = {"AMD Ryzen 9 5900X", "AMD Ryzen 7 5700X", "AMD Ryzen 5 5600X", "Intel i9-12900K", "Intel i7-12700K", "Intel i5-12600K" };
+            double[] processorPrices = {1410, 1270, 1200, 1590, 1400, 1280};
+            String[] memory = { "8 GB", "16 GB" };
+            double[] memoryPrices = { };
+
+            Console.WriteLine("Select a processor:");
+            Console.WriteLine();
+            Console.WriteLine("{0,-20} {1,5}\n", "Processor", "Price");
+
+            for (int currProcessor = 0; currProcessor < processors.Length; currProcessor++)
+            {
+                Console.WriteLine("{0,-20} {1,5:N1}", processors[currProcessor], processorPrices[currProcessor]);
+            }
+
+            chosenProcessor = Console.ReadLine(); 
+
+
         }
     }
 }
