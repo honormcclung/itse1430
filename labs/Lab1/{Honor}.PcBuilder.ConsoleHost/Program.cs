7adc75e630166e8533fd998a9100a516b9c3561c
Honor McClung <honor.mcclung@my.tccd.edu>
2022-09-19 21:02:26 -0500

    Added new order method.

--- labs/Lab1/{Honor}.PcBuilder.ConsoleHost/Program.cs
+++ labs/Lab1/{Honor}.PcBuilder.ConsoleHost/Program.cs
@@ -66,17 +66,17 @@ namespace Lab1
 
         static void newOrder ()
         {
-            int cartTotal = 0;
+            double cartTotal = 0;
             String chosenProcessor = "";
             String chosenMemory = "";
+            Boolean isProcessorValid = false;
+            Boolean isMemoryValid = false;
 
             String[] processors = {"AMD Ryzen 9 5900X", "AMD Ryzen 7 5700X", "AMD Ryzen 5 5600X", "Intel i9-12900K", "Intel i7-12700K", "Intel i5-12600K" };
             double[] processorPrices = {1410, 1270, 1200, 1590, 1400, 1280};
-            String[] memory = { "8 GB", "16 GB" };
-            double[] memoryPrices = { };
+            String[] memory = { "8 GB", "16 GB", "32 GB", "64 GB", "128 GB"};
+            double[] memoryPrices = {30, 40, 90, 410, 600};
 
-            Console.WriteLine("Select a processor:");
-            Console.WriteLine();
             Console.WriteLine("{0,-20} {1,5}\n", "Processor", "Price");
 
             for (int currProcessor = 0; currProcessor < processors.Length; currProcessor++)
@@ -84,8 +84,56 @@ namespace Lab1
                 Console.WriteLine("{0,-20} {1,5:N1}", processors[currProcessor], processorPrices[currProcessor]);
             }
 
-            chosenProcessor = Console.ReadLine(); 
+            do
+            {
+                Console.WriteLine("Select a processor:");
+                chosenProcessor = Console.ReadLine();
 
+                for (int currProcessor = 0; currProcessor < processors.Length; currProcessor++)
+                {
+                    if (chosenProcessor.Equals(processors[currProcessor]))
+                    {
+                        cartTotal = cartTotal + processorPrices[currProcessor];
+                        isProcessorValid = true;
+                    }
+                }
+
+                if (isProcessorValid == false)
+                {
+                    Console.WriteLine("Invalid Input. Try again.");
+                }
+            } while (isProcessorValid == false);
+
+
+            Console.WriteLine("{0,-20} {1,5}\n", "Memory", "Price");
+
+            for (int currMemory = 0; currMemory < memory.Length; currMemory++)
+            {
+                Console.WriteLine("{0,-20} {1,5:N1}", memory[currMemory], memoryPrices[currMemory]);
+            }
+
+            do
+            {
+                Console.WriteLine("Select a memory option:");
+                chosenMemory = Console.ReadLine();
+
+                for (int currMemory = 0; currMemory < memory.Length; currMemory++)
+                {
+                    if (chosenMemory.Equals(memory[currMemory]))
+                    {
+                        cartTotal = cartTotal + memoryPrices[currMemory];
+                        isMemoryValid = true;
+                    }
+                }
+
+                if (isMemoryValid == false)
+                {
+                    Console.WriteLine("Invalid Input. Try again.");
+                }
+            } while (isMemoryValid == false);
+
+            Console.WriteLine();
+            Console.WriteLine("Cart Total: $" + cartTotal);
 
         }
     }
