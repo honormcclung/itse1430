14b24a9bd4a32c0b83385746b6bd71d8a1904e5d
honormcclung <46303214+honormcclung@users.noreply.github.com>
2022-10-18 14:22:52 -0700

    Added Lab 2

---
+++ labs/Lab2/Honor.CharacterCreator.ConsoleHost/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ * ITSE 1430 Sample Implementation
+ * Computer Builder
+ */
+
+Console.WriteLine("Iste 1430");
+Console.WriteLine("Lab 2");
+Console.WriteLine("Honor McClung");
+
+
+
+
+
